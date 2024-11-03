70a4ba0f65355ca4e32370b5b703379c64124452
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-11-03 19:05:14 +0530

    Added code to understand ContextAPI and Reducer Method

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo/AddTodo'
 
+import TodoList from './components/TodoList/TodoList'
+
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addTodo, deleteTodo, editTodo, finishTodo} from './slice/TodoSlice';
 function App() {
-  const [count, setCount] = useState(0)
-
+  const dispatch = useDispatch();
+  const actions = bindActionCreators({ addTodo, deleteTodo, editTodo, finishTodo}, dispatch);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+        <AddTodo addTodo={actions.addTodo} />
+        <TodoList deleteTodo={actions.deleteTodo} editTodo={actions.editTodo} finishTodo={actions.finishTodo} />
+   </>
   )
 }
 
